1225912fe19fc280452be3edcc4256d64b4d1b0f
santiiago18 <scardaya@myuax.com>
2026-03-10 09:54:40 +0100

    feat (GestionMision): Se crea metodo listarMisiones y buscarMision

--- src/platosComidaDos/Mision.java
+++ src/platosComidaDos/Mision.java
@@ -1,3 +1,5 @@
+package platosComidaDos;
+
 public class Mision {
 
     private int id;
@@ -8,7 +10,7 @@ public class Mision {
     private boolean completada;
 
     /**
-     *  Constructor Mision
+     *  Constructor platosComidaDos.Mision
      * @param id identificador de la mision
      * @param nombre nombre de la mision
      * @param dificultad dificultad de la mision
@@ -75,13 +77,13 @@ public class Mision {
 
 
     /**
-     * Texto de todos los atributos de la clase Mision
-     * @return texto con todos los atributos de la clase Mision
+     * Texto de todos los atributos de la clase platosComidaDos.Mision
+     * @return texto con todos los atributos de la clase platosComidaDos.Mision
      */
     public String toString(){
         String resultado;
 
-        resultado = String.format(new String("Mision: id: %d, nombre: %s, dificultas: %d, nivel recomendad: %d,            recompensa experiencia: %d, completada: %b"),
+        resultado = String.format(new String("platosComidaDos.Mision: id: %d, nombre: %s, dificultas: %d, nivel recomendad: %d,            recompensa experiencia: %d, completada: %b"),
             id, nombre, dificultad, nivelRecomendado, recompensaExperiencia, completada);
 
         return resultado;
